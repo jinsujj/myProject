0c98c76bbde0b185e60a229ec8de71254462e380
jinsujj <wlstncjs1234@naver.com>
2024-02-23 19:30:00 +0900

    main args order change

--- src/main/java/com/example/myProject/App.java
+++ src/main/java/com/example/myProject/App.java
@@ -12,7 +12,7 @@ public class App
 
     public static void main( String[] args )
     {
-        if(args.length == 0 || args[0].equals("generator")){
+        if(args[0].equals("generator")|| args.length == 0){
             TestDataGenerator testDataGenerator = new TestDataGenerator(CUSTOMER_COUNT, SIMULATANEOUS_CUSTOMER);
             testDataGenerator.simulateCustomerBehavior();
         }
