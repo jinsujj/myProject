4a50532b2f3c2e8d255523b9d4edc3184350bfab
jinsujj <wlstncjs1234@naver.com>
2024-03-25 20:40:51 +0900

    delete 폴더

--- src/main/java/com/example/myProject/common/domain/FinancialAction.java
+++
@@ -1,5 +0,0 @@
-package com.example.myproject.common.domain;
-
-public enum FinancialAction {
-    SESSION_START, SIGNUP, OPEN_ACCOUNT, DEPOSIT, WITHDRAWAL, TRANSFER
-}
--- src/main/java/com/example/myProject/common/response/dto/BaseInfo.java
+++
@@ -1,7 +0,0 @@
-package com.example.myproject.common.response.dto;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-public class BaseInfo {
-    protected transient ObjectMapper mapper = new ObjectMapper();
-}
